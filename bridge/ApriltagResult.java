651905c6bff6094119af44bab288fabe43aab53f
DanPeled <98838880+DanPeled@users.noreply.github.com>
2025-01-30 16:32:54 +0200

    Actual results

---
+++ bridge/ApriltagResult.java
@@ -0,0 +1,105 @@
+package frc.robot.subsystems.vision;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import edu.wpi.first.math.geometry.Pose3d;
+
+public class ApriltagResult {
+    @JsonProperty("timestamp")
+    private double timestamp;
+
+    @JsonProperty("tag_family")
+    private String tagFamily;
+
+    @JsonProperty("tag_id")
+    private int tagID;
+
+    @JsonProperty("hamming")
+    private double hamming;
+
+    @JsonProperty("decision_margin")
+    private double decisionMargin;
+
+    @JsonProperty("homography")
+    private double[][] homongraphy;
+
+    @JsonProperty("center")
+    private double[] center;
+
+    @JsonProperty("pose_R")
+    private double[][] pose_R;
+
+    @JsonProperty("pose_t")
+    private double[][] pose_t;
+
+    @JsonProperty("corners")
+    private double[][] corners;
+
+    @JsonProperty("pose_err")
+    private double poseError;
+
+    @JsonDeserialize(using = Pose3dDeserializer.class)
+    private Pose3d robotPose;
+
+    @JsonDeserialize(using = Pose3dDeserializer.class)
+    private Pose3d tagRelativePose;
+
+    @JsonDeserialize(using = Pose3dDeserializer.class)
+    private Pose3d tagFieldPose;
+
+    public double getTimestamp() {
+        return timestamp;
+    }
+
+    public String getTagFamily() {
+        return tagFamily;
+    }
+
+    public int getTagID() {
+        return tagID;
+    }
+
+    public double getHamming() {
+        return hamming;
+    }
+
+    public double getDecisionMargin() {
+        return decisionMargin;
+    }
+
+    public double[][] getHomongraphy() {
+        return homongraphy;
+    }
+
+    public double[] getCenter() {
+        return center;
+    }
+
+    public double[][] getPose_R() {
+        return pose_R;
+    }
+
+    public double[][] getPose_t() {
+        return pose_t;
+    }
+
+    public double[][] getCorners() {
+        return corners;
+    }
+
+    public double getPoseError() {
+        return poseError;
+    }
+
+    public Pose3d getRobotPose() {
+        return robotPose;
+    }
+
+    public Pose3d getTagRelativePose() {
+        return tagRelativePose;
+    }
+
+    public Pose3d getTagFieldPose() {
+        return tagFieldPose;
+    }
+}
--- bridge/Synapse.java
+++ bridge/Synapse.java
@@ -1,4 +1,13 @@
-package frc.vision;
+package frc.robot.subsystems.vision;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
@@ -11,46 +20,38 @@ import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class Synapse {
-  private NetworkTable m_table = NetworkTableInstance.getDefault().getTable("Synapse");
+  private static NetworkTable m_table = NetworkTableInstance.getDefault().getTable("Synapse");
 
-  public Synapse() {
-  }
-
-  public void setPipeline(int cameraIndex, long pipelineIndex) {
+  public static void setPipeline(int cameraIndex, long pipelineIndex) {
     getPipelineEntry(cameraIndex).setInteger(pipelineIndex);
   }
 
-  public long getPipeline(int cameraIndex) {
+  public static long getPipeline(int cameraIndex) {
     return getPipelineEntry(cameraIndex).getInteger(-1);
   }
 
-  public StampedBotPose getRobotPose(int cameraIndex) {
-    double[] poseArr = getDataEntryTable(cameraIndex, "field").getEntry("Robot").getDoubleArray(new double[3]);
-    return new StampedBotPose(pose2DFromArr(poseArr), getDataEntry(cameraIndex, "timestamp").getDouble(-1));
-  }
-
-  public Pose3d getDeltaTagPose(int cameraIndex) {
+  public static Pose3d getDeltaTagPose(int cameraIndex) {
     double[] poseArr = getDataEntry(cameraIndex, "deltaTagPose").getDoubleArray(new double[6]);
     return pose3DFromArr(poseArr);
   }
 
-  public NetworkTableEntry getDataEntry(int cameraIndex, String dataKey) {
+  public static NetworkTableEntry getDataEntry(int cameraIndex, String dataKey) {
     return getDataResultsTable(cameraIndex).getEntry(dataKey);
   }
 
-  public NetworkTable getDataEntryTable(int cameraIndex, String dataKey) {
+  public static NetworkTable getDataEntryTable(int cameraIndex, String dataKey) {
     return getDataResultsTable(cameraIndex).getSubTable(dataKey);
   }
 
-  public NetworkTable getDataResultsTable(int cameraIndex) {
+  public static NetworkTable getDataResultsTable(int cameraIndex) {
     return getCameraTable(cameraIndex).getSubTable("data");
   }
 
-  private NetworkTableEntry getPipelineEntry(int cameraIndex) {
+  private static NetworkTableEntry getPipelineEntry(int cameraIndex) {
     return getCameraTable(cameraIndex).getEntry("pipeline");
   }
 
-  private NetworkTable getCameraTable(int cameraIndex) {
+  private static NetworkTable getCameraTable(int cameraIndex) {
     return m_table.getSubTable(String.format("camera%d", cameraIndex));
   }
 
@@ -62,5 +63,33 @@ public class Synapse {
   public static Pose2d pose2DFromArr(double[] arr) {
     return new Pose2d(
         new Translation2d(arr[0], arr[1]), Rotation2d.fromDegrees(arr[2]));
+  }
+
+  public static List<ApriltagResult> getResults(int cameraIndex) throws IllegalArgumentException {
+    String jsonString = getDataEntry(cameraIndex, "results").getString("[{}]");
+    if (jsonString.equals("[{}]")) {
+      return new ArrayList<>();
+    } else {
+      ObjectMapper objectMapper = new ObjectMapper();
+      List<ApriltagResult> resultList;
+      try {
+        Map<String, Object> resultMap = objectMapper.readValue(jsonString, new TypeReference<Map<String, Object>>() {
+        });
+
+        String type = (String) resultMap.get("type");
+        if (type.equals("apriltag")) {
+          resultList = objectMapper.convertValue(resultMap.get("data"), new TypeReference<List<ApriltagResult>>() {
+          });
+          return resultList;
+        } else {
+          throw new IllegalArgumentException("[Synapse] : Error - Non-existent result type: " + type);
+        }
+      } catch (JsonMappingException e) {
+        e.printStackTrace();
+      } catch (JsonProcessingException e) {
+        e.printStackTrace();
+      }
+    }
+    return new ArrayList<>();
   }
 }
