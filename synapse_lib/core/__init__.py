88f84d1ccc4e5325f81676f89bd00f3c2f0a4eba
DanPeled <98838880+DanPeled@users.noreply.github.com>
2025-03-15 15:08:55 +0200

    Removed log import from core __init__.py (#36)

--- synapse_lib/core/__init__.py
+++ synapse_lib/core/__init__.py
@@ -1,5 +1,4 @@
-from .log import log, err
 from .synapse import Synapse
 from .pipline_handler import PipelineHandler
 
-__all__ = ["Synapse", "PipelineHandler", "log", "err"]
+__all__ = ["Synapse", "PipelineHandler"]
