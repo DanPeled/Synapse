86fb4357fce05abf3d0ab9090b1f7e808d824c03
DanPeled <98838880+DanPeled@users.noreply.github.com>
2025-07-09 12:21:07 +0300

    added tests for automatic pipeline bind

--- synapse_core/src/synapse/core/runtime_handler.py
+++ synapse_core/src/synapse/core/runtime_handler.py
@@ -919,7 +919,7 @@ class RuntimeManager:
         """
         if cameraIndex not in self.cameraHandler.cameras.keys():
             log.err(
-                f"Invalid cameraIndex {cameraIndex}. Must be in range(0, {len(self.cameraHandler.cameras.keys()) - 1})."
+                f"Invalid cameraIndex {cameraIndex}. Must be in {list(self.cameraHandler.cameras.keys())})."
             )
             return
 
