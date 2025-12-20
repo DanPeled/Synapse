14d961f894eeb2ce5db1fcbae26032200f3e6982
DanPeled <danpeled31@gmail.com>
2025-12-20 17:31:45 +0200

    updated synapselib version

--- synapse_lib/py/setup.py
+++ synapse_lib/py/setup.py
@@ -10,7 +10,7 @@ SOURCE_DIR: Final[str] = "src"
 
 setup(
     name="synapsefrclib",
-    version="2025.0.0a3",
+    version="2025.0.1",
     packages=find_packages(where=SOURCE_DIR),
     package_dir={"": SOURCE_DIR},
     python_requires=">=3.9, <3.12",
