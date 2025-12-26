a4dccf2cfa7b7bfacacdc86f38fcfd205741b9c9
DanPeled <danpeled31@gmail.com>
2025-12-26 18:14:04 +0200

    added pypi descriptors

--- setup.py
+++ setup.py
@@ -57,6 +57,9 @@ for name, path in modules.items():
 setup(
     name="synapsefrc",
     version=SYNAPSE_VERSION,
+    description="Dynamic vision processing system for FRC coprocessors",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
     packages=allModules,
     package_dir=moduleDirs,
     python_requires=">=3.9, <3.12",
